8e1b7c37857f1092e3f872ce5bf42de8e27b1be9
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-08-31 14:37:50 +0530

    Automation fix

--- base/automate.sh
+++ base/automate.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -e
 
-# echo "Setting up namespace for deployment of CloudNative PG Cluster..."
-# kubectl apply -f namespace.yml
+echo "Setting up namespace for deployment of CloudNative PG Cluster..."
+kubectl apply -f namespace.yml
 
-# echo "Fetching MinIO Certificate Authority..."
-# MINIO_CA_SECRET_JSON=$(kubectl get secret -n photoatom-object-storage photoatom-ca-certificate-tls -o json)
+echo "Fetching MinIO Certificate Authority..."
+MINIO_CA_SECRET_JSON=$(kubectl get secret -n photoatom-object-storage photoatom-ca-certificate-tls -o json)
 
-# echo "Adding Certificate Authority Details into manifest..."
-# CA_CRT=$(echo "$MINIO_CA_SECRET_JSON" | jq -rc '.data."ca.crt"')
-# TLS_CRT=$(echo "$MINIO_CA_SECRET_JSON" | jq -rc '.data."tls.crt"')
-# TLS_KEY=$(echo "$MINIO_CA_SECRET_JSON" | jq -rc '.data."tls.key"')
+echo "Adding Certificate Authority Details into manifest..."
+CA_CRT=$(echo "$MINIO_CA_SECRET_JSON" | jq -rc '.data."ca.crt"')
+TLS_CRT=$(echo "$MINIO_CA_SECRET_JSON" | jq -rc '.data."tls.crt"')
+TLS_KEY=$(echo "$MINIO_CA_SECRET_JSON" | jq -rc '.data."tls.key"')
 
-# sed -i "s|MINIO_TLS_CERT_HERE|$TLS_CRT|g" minio-ca.yml
-# sed -i "s|MINIO_TLS_KEY_HERE|$TLS_KEY|g" minio-ca.yml
-# sed -i "s|MINIO_CA_CERT_HERE|$CA_CRT|g" minio-ca.yml
+sed -i "s|MINIO_TLS_CERT_HERE|$TLS_CRT|g" minio-ca.yml
+sed -i "s|MINIO_TLS_KEY_HERE|$TLS_KEY|g" minio-ca.yml
+sed -i "s|MINIO_CA_CERT_HERE|$CA_CRT|g" minio-ca.yml
 
-# echo "Setting up MinIO Certificate Authority..."
-# kubectl apply -f minio-ca.yml
+echo "Setting up MinIO Certificate Authority..."
+kubectl apply -f minio-ca.yml
 
 echo "Generating Access Keys for MinIO..."
 MINIO_ACCESS_KEY="postgres-access"
@@ -42,7 +42,7 @@ mc admin user svcacct add \
 kill -9 "$PROCESS_NUMBER"
 
 echo "Setting up MinIO Access Keys for Postgres..."
-sed -i "s|MINIO_ACCESS_KEY_HERE|$(echo $MINIO_ACCESS_KEY | base64)|g" minio-access-key.yml
-sed -i "s|MINIO_ACCESS_SECRET_HERE|$(echo $MINIO_ACCESS_SECRET | base64)|g" minio-access-key.yml
+sed -i "s|MINIO_ACCESS_KEY_HERE|$(echo -n $MINIO_ACCESS_KEY | base64)|g" minio-access-key.yml
+sed -i "s|MINIO_ACCESS_SECRET_HERE|$(echo -n $MINIO_ACCESS_SECRET | base64)|g" minio-access-key.yml
 
 kubectl apply -f minio-access-key.yml
