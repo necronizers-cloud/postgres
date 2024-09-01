fd9cf415ed731f47342c38e51258850fd74ca03d
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-09-01 15:31:23 +0530

    Remove automation for the certs

--- src/automate.sh
+++ src/automate.sh
@@ -1,21 +1,5 @@
 #!/bin/bash -e
 
-echo "Setting up Client Certificates for Postgres..."
-kubectl apply -f client-ca.yml
-sleep 10
-kubectl apply -f client-ca-issuer.yml
-sleep 10
-kubectl apply -f client-cert.yml
-sleep 10
-
-echo "Setting up Server Certificates for Postgres..."
-kubectl apply -f server-ca.yml
-sleep 10
-kubectl apply -f server-ca-issuer.yml
-sleep 10
-kubectl apply -f server-cert.yml
-sleep 10
-
 echo "Setting up user credentials for Postgres..."
 PHOTOATOM_CREDS_USERNAME="photoatom"
 PHOTOATOM_CREDS_PASSWORD="$(openssl rand -base64 16)"
