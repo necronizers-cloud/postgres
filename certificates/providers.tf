14d95bec11d7b4075b5bf57cf2d3acb0bda04146
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-09-08 15:11:25 +0530

    Certificates moved to OpenTofu

---
+++ certificates/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "opentofu/kubernetes"
+      version = "2.32.0"
+    }
+  }
+
+  backend "kubernetes" {
+    secret_suffix = "certificates.postgres"
+    config_path   = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
